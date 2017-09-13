aa24e19025e03658248a8b7d8706997ba41f2282
Joe Li <joe.li@okchem.com>
2017-09-13 09:08:19 +0800

    refine

--- routes/captcha.js
+++ routes/captcha.js
@@ -4,11 +4,20 @@ const router = express.Router()
 /* eslint-enable new-cap */
 const svgCaptcha = require('svg-captcha')
 const jwt = require('jsonwebtoken')
-// const uuidv4 = require('uuid/v4')
 const secret = 'secret'
+const defaultOptions = {
+  ignoreChars: '0o1ilL',
+  noise: 2,
+  size: 5,
+  width: 150, // width of captcha
+  height: 35, // height of captcha
+  fontSize: 35,
+}
+const ERR_WRONG_CODE = 'codeWrong'
+const ERR_EXPIRED_CODE = 'codeExpired'
 /* GET home page. */
 router.get('/', (req, res) => {
-  const captcha = svgCaptcha.create()
+  const captcha = svgCaptcha.create(defaultOptions)
   const jwtToken = jwt.sign({
     exp: Math.floor(Date.now() / 1000) + (60 * 10), // 1 minute
     data: { c: captcha.text },
@@ -17,8 +26,14 @@ router.get('/', (req, res) => {
   return res.render('captcha', { token: jwtToken, captchaSVG: captcha.data })
 })
 
+router.get('/svg', (req, res) => {
+  const captcha = svgCaptcha.create(defaultOptions)
+  res.type('svg')
+  return res.status(200).send(captcha.data)
+})
+
 router.post('/verify', (req, res) => {
-  let errCode = 'codeWrong'
+  let errCode = ERR_WRONG_CODE
   try {
     const decoded = jwt.verify(req.body.sCap, secret)
     const captchaText = decoded.data.c
@@ -27,7 +42,7 @@ router.post('/verify', (req, res) => {
     }
   } catch (err) {
     if (err.name === 'TokenExpiredError') {
-      errCode = 'codeExpire'
+      errCode = ERR_EXPIRED_CODE
     }
     return res.status(400).json({ err: errCode })
   }
